7f6525d9de10a47d1d2a8ce7b42511ef68ffec07
Phillip Alday <me@phillipalday.com>
2022-07-27 22:58:56 -0500

    oops

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ using MixedModelsSerialization
 makedocs(; modules=[MixedModelsSerialization],
          authors="Phillip Alday and contributors",
          repo="https://github.com/JuliaMixedModels/MixedModelsSerialization.jl/blob/{commit}{path}#{line}",
-         sitename="Effects.jl",
+         sitename="MixedModelsSerialization.jl",
          format=Documenter.HTML(; prettyurls=get(ENV, "CI", "false") == "true",
                                 canonical="https://juliamixedmodels.github.io/MixedModelsSerialization.jl",
                                 assets=String[]),
