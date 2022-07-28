4ef9be3427169b49cbeea351276de9b90706b471
Documenter.jl <documenter@juliadocs.github.io>
2022-07-28 02:26:07 +0000

    build based on 9f78fa1

---
+++ previews/PR1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"api/","page":"API","title":"API","text":"CurrentModule = MixedModelsSerialization","category":"page"},{"location":"api/#API","page":"API","title":"API","text":"","category":"section"},{"location":"api/","page":"API","title":"API","text":"","category":"page"},{"location":"api/","page":"API","title":"API","text":"Modules = [MixedModelsSerialization]","category":"page"}]
+}
